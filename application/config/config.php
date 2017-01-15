6c35b158d73aeed4cfaa63fe6393606d3eec0a67
Caro Bottino <carobottino888@gmail.com>
2017-01-15 17:59:50 -0300

    antes de volver a empezar

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/hazquesuceda';
+$config['base_url'] = 'http://www.hazquesuceda.org/';
 
 /*
 |--------------------------------------------------------------------------
