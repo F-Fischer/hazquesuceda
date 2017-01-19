d2b9779a1a2181fdb01e26f7bf1d6d02e3ac7c80
Caro Bottino <carobottino888@gmail.com>
2017-01-19 02:50:36 -0300

    Fix envio mail inversor

--- application/controllers/RegistroInversor.php
+++ application/controllers/RegistroInversor.php
@@ -30,21 +30,17 @@ class RegistroInversor extends CI_Controller
         $this->load->view('commons/footer');
     }
 
-    public function registrar(){
-
-        //Validaciones emprendedor
-
+    public function registrar()
+    {
         $this->form_validation->set_rules('nombre','Nombre', 'trim|required', array('required' => 'No ingreso nombre'));
         $this->form_validation->set_rules('apellido','Apellido', 'trim|required',array('required' => 'No ingreso apellido'));
         $this->form_validation->set_rules('username','Usuario', 'trim|required|callback_validate_username', array('required' => 'Debe ingresar un nombre de usuario',
-            'dvalidate_username' => 'El usuario ya existe'));
+            'validate_username' => 'El usuario ya existe'));
         $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[6]', array('min_length[6]' => 'La contrase�a debe tener mas de 6 caracteres',
             'required' => 'Debe ingresar una contrase�a'));
         $this->form_validation->set_rules('passconf', 'Password Confirmation', 'trim|required|matches[password]', array('matches[password]' => 'Las contrase�as no son iguales'));
         $this->form_validation->set_rules('mail', 'E-Mail', 'trim|required|valid_email|callback_validate_email', array('required' => 'Debe ingresar una cuenta de e-mail',
             'valid_email' => 'Debe ingresar un mail'));
-
-        //Valores obtenidos del form emprendedor
 
         $nombre = $this->input->post('nombre');
         $apellido = $this->input->post('apellido');
@@ -85,18 +81,10 @@ class RegistroInversor extends CI_Controller
                     $ri->setIdRubro($rubro);
                     $ri->insertRubroInteres();
                 }
+                $this->send_email($mail, $username, $password);
                 redirect('exitoinversor');
             }
         }
-    }
-
-    public function exito()
-    {
-        $data['username'] =  null;
-
-        $this->load->view('commons/header',$data);
-        $this->load->view('postregistro');
-        $this->load->view('commons/footer');
     }
 
     public function validarUsuario()
@@ -144,5 +132,45 @@ class RegistroInversor extends CI_Controller
         {
             return true;
         }
+    }
+
+    public function send_email($email, $username, $password) {
+        $to = $email;
+        $subject = "Bienvenido a Haz que suceda!";
+
+        $message = "
+                    <html>
+                        <head>
+                            <title>HTML email</title>
+                        </head>
+                        <body>
+                            <div class=\"jumbotron\">
+                              <h1>Bienvenido a Haz que suceda!</h1>
+                              <label>Tu usuario es: <strong>" . $username. " </strong></label>
+                              <br>
+                              <label>Y tu contraseña es: <strong>" . $password . " </strong></label>
+                              <p><a href=\"http://www.hazquesuceda.org/activar/". $username ."\" >Activar mi cuenta!</a></p>
+                            </div>
+                        </body>
+                    </html>
+                    ";
+
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // More headers
+        $headers .= 'From: <soporte@hazquesuceda.org>' . "\r\n";
+
+        mail($to,$subject,$message,$headers);
+    }
+
+    public function exito()
+    {
+        $data['username'] =  null;
+
+        $this->load->view('commons/header',$data);
+        $this->load->view('postregistro');
+        $this->load->view('commons/footer');
     }
 }
