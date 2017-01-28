07e2b06070fce63a1d670bb01e17eaf5f85acf28
Caro Bottino <carobottino888@gmail.com>
2017-01-28 02:17:04 -0300

    Flujo separado para crear proyecto

--- application/views/emprendedor/crear_proyecto.php
+++ application/views/emprendedor/crear_proyecto.php
@@ -14,81 +14,60 @@
 
     </div>
 
-    <div class="col-md-3">
-
-        <ul class="nav nav-pills nav-stacked" >
-
-            <li role="presentation" ><a href=" <?php echo base_url('emprendedor'); ?> ">Ver todos los proyectos</a></li>
-            <li role="presentation" class="active dropdown-menu"><a href="crearproyecto">Crear proyecto</a>
-
-                    <ul class="nav nav-pills nav-stacked" >
-                        <li role="presentation" id="caro"><a href="crearproyecto">Nuevo proyecto</a></li>
-                        <li role="presentation" class="disabled" ><a href="video">Video</a></li>
-                        <li role="presentation" class="disabled"><a href="imagenes">Imágenes</a></li>
-                        <li role="presentation" class="disabled"><a href="archivo">Archivo</a></li>
-                    </ul>
-
-            </li>
-            <li role="presentation"><a href=" <?php echo base_url('misproyectos'); ?> ">Ver todos mis proyectos</a></li>
-            <li role="presentation"><a href=" <?php echo base_url('micuentaE'); ?> ">Mi cuenta</a></li>
-
-        </ul>
-
-    </div>
+    <ul class="nav nav-tabs nav-justified">
+        <li role="presentation" class="active"><a>Tu proyecto</a></li>
+        <li role="presentation" class="disabled"><a>Video</a></li>
+        <li role="presentation" class="disabled"><a>Imágenes</a></li>
+        <li role="presentation" class="disabled"><a>Archivo</a></li>
+    </ul>
 
     <div class="col-md-9">
 
-        <div class="panel panel-default">
-            <div class="panel-body">
+        <div class="col-md-6">
 
-                <div class="col-md-6">
+            <?php
+            echo form_open('proyectocontroller/crearProyecto');
 
-                    <?php
-                    echo form_open('proyectocontroller/crearProyecto');
+            echo '<div class="form-group">'.form_label('Título del proyecto ').form_error('nombre', '<div class="error" style="color:red; float: right;">', '</div>');
 
-                    echo '<div class="form-group">'.form_label('Título del proyecto ').form_error('nombre', '<div class="error" style="color:red; float: right;">', '</div>');
+            $data = array (
+                'id' => 'inputNombre',
+                'name' => 'nombre',
+                'class' => 'form-control',
+                'value' => set_value('nombre')
+            );
 
-                    $data = array (
-                        'id' => 'inputNombre',
-                        'name' => 'nombre',
-                        'class' => 'form-control',
-                        'value' => set_value('nombre')
-                    );
+            echo form_input($data).'</div>';
 
-                    echo form_input($data).'</div>';
+            echo '<div class="form-group">'.form_label('Descripción ').form_error('descripcion', '<div class="error" style="color:red; float: right;">', '</div>');
 
-                    echo '<div class="form-group">'.form_label('Descripción ').form_error('descripcion', '<div class="error" style="color:red; float: right;">', '</div>');
+            $data = array (
+                'id' => 'inputDescripcion',
+                'name' => 'descripcion',
+                'class' => 'form-control',
+                'value' => set_value('descripcion')
+            );
 
-                    $data = array (
-                        'id' => 'inputDescripcion',
-                        'name' => 'descripcion',
-                        'class' => 'form-control',
-                        'value' => set_value('descripcion')
-                    );
+            echo form_textarea($data).'</div>';
 
-                    echo form_textarea($data).'</div>';
+            echo '<div class="form-group">'.form_error('rubro', '<div class="error" style="color:red; float: right;">', '</div>');
 
-                    echo '<div class="form-group">'.form_error('rubro', '<div class="error" style="color:red; float: right;">', '</div>');
+            echo '<select id="comboRubros" name="comboRubros" class="col-md-6" class="form-control">';
+            foreach($rubros as $rubro)
+                echo '<option value="'.$rubro->ID_rubro.'">'.$rubro->nombre.'</option>';
+            echo '</select><br>';
 
-                    echo '<select id="comboRubros" name="comboRubros" class="col-md-6" class="form-control">';
-                        foreach($rubros as $rubro)
-                        echo '<option value="'.$rubro->ID_rubro.'">'.$rubro->nombre.'</option>';
-                    echo '</select><br>';
+            $data = array(
+                'id' => 'btnCrearProyecto',
+                'class' => 'btn btn-default',
+                'value' => 'Crear proyecto!',
+            );
 
-                    $data = array(
-                        'id' => 'btnCrearProyecto',
-                        'class' => 'btn btn-default',
-                        'value' => 'Crear proyecto!',
-                    );
+            echo '<br>'.form_submit($data,'Crear proyecto!');
 
-                    echo '<br>'.form_submit($data,'Crear proyecto!');
+            echo form_close();
+            ?>
 
-                    echo form_close();
-                    ?>
-
-                </div>
-
-            </div>
         </div>
 
     </div>
