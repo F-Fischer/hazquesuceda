b721b25982112d3e32d20b8f90c7b91cbbb0ef99
F-Fischer <francisco.fischer92@gmail.com>
2017-01-31 16:47:42 -0300

    Acciones de emprendedor sobre proyectos

--- assets/js/emprendedor/acciones.proyectos.js
+++ assets/js/emprendedor/acciones.proyectos.js
@@ -3,60 +3,60 @@ $(document).ready(function (){
     $('body').on('click','.modificar', function(){
         if(confirm('Desea modificar este proyecto?')) {
             var idProyecto = $(this).attr('value');
-            /*$.ajax({
-                url: 'admin/aceptarproyecto',
-                data: {idProyecto: idProyecto},
-                method: "get",
-                dataType: "json",
-                success: function (e) {
-                    $("#nombreEstado" + idProyecto).html("activo");
-                }
-            });*/
+            $.ajax({
+                 url: 'emprendedor/modificarproyecto',
+                 data: {idProyecto: idProyecto},
+                 method: "get",
+                 dataType: "json",
+                 success: function (e) {
+                 
+             }
+             });
         }
     });
 
     $('body').on('click','.clausurar', function(){
         if(confirm('Desea clausurar este proyecto?')) {
             var idProyecto = $(this).attr('value');
-            /*$.ajax({
-             url: 'admin/aceptarproyecto',
+            $.ajax({
+             url: 'emprendedor/clausurarproyecto',
              data: {idProyecto: idProyecto},
              method: "get",
              dataType: "json",
              success: function (e) {
-             $("#nombreEstado" + idProyecto).html("activo");
+             
              }
-             });*/
+             });
         }
     });
 
     $('body').on('click','.renovar', function(){
         if(confirm('Desea renovar este proyecto?')) {
             var idProyecto = $(this).attr('value');
-            /*$.ajax({
-             url: 'admin/aceptarproyecto',
+            $.ajax({
+             url: 'emprendedor/renovarproyecto',
              data: {idProyecto: idProyecto},
              method: "get",
              dataType: "json",
              success: function (e) {
-             $("#nombreEstado" + idProyecto).html("activo");
+             
              }
-             });*/
+             });
         }
     });
 
     $('body').on('click','.finalizar', function(){
         if(confirm('Desea finalizar este proyecto?')) {
             var idProyecto = $(this).attr('value');
-            /*$.ajax({
-             url: 'admin/aceptarproyecto',
+            $.ajax({
+             url: 'emprendedor/finalizarproyecto',
              data: {idProyecto: idProyecto},
              method: "get",
              dataType: "json",
              success: function (e) {
-             $("#nombreEstado" + idProyecto).html("activo");
+            
              }
-             });*/
+             });
         }
     });
 
